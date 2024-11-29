f98cceedb1f96998f7d031e55641321ac21404a6
Neryss <neryssistos@gmail.com>
2024-11-29 14:27:13 +0100

    ADD: workaround for keystates

--- app.ts
+++ app.ts
@@ -3,7 +3,7 @@ import style from "./style.scss"
 import Bar from "./widget/Bar"
 
 App.start({
-//     css: style,
+    css: style,
 //     main() {
 //         App.get_monitors().map(Bar)
 //     },
--- widget/MyInput.tsx
+++ widget/MyInput.tsx
@@ -5,8 +5,16 @@ let   last_key_time = 0;
 
 
 export function MyInput() {
+    interface   Specs
+    {
+        ctrl: boolean,
+        alt: boolean,
+        shift: boolean
+    };
+
     let     timer: any;
     let     keys_buffer: Array<string> = [];
+    let     specs: Specs = {ctrl:false, alt:false, shift: false};
 
     function    resetTimer() {
         if (timer)
@@ -20,39 +28,73 @@ export function MyInput() {
     const   letter = Variable("")
 
     function updateDisplay(newKey: Array<string>) {
-        // let tmp: string = "";
-        // for (let i = 0; i < newKey.length; i++)
-        //     tmp += newKey[i] + " ";
-        letter.set(newKey.join("|"));
+        if (specs.ctrl)
+            letter.set(newKey.join("+"));
+        else
+            letter.set(newKey.join("|"));
     }
 
 
     subprocess("sudo keyd monitor", (output) => {
         if (!output || output.includes('device added:') || !output.includes("Razer Huntsman Mini")) return;
-        console.log("Callback triggered");
         let rest = output.slice(output.lastIndexOf("\t") + 1);
         let item = rest.split(' ');
+        let     string_buffer: string = "";
 
         if (item[1] === "down")
         {
-            const   current_time = Date.now();
-            const   time_since_last_press = current_time - last_key_time;
-            last_key_time = current_time;
+            // const   current_time = Date.now();
+            // last_key_time = current_time;
 
             resetTimer();
-            keys_buffer.push(item[0]);
+            if (specs.ctrl || specs.shift || specs.alt)
+                keys_buffer.push("+");
+            else
+                keys_buffer.push("|");
+            if (item[0] === "space")
+                keys_buffer.push("[SP]");
+            else if (item[0] === "backspace")
+                keys_buffer.push("[BCKSP]");
+            else if (item[0] === "enter")
+                keys_buffer.push("[ENTER]");
+            else if (item[0] === "leftshift" || item[0] === "rightshift")
+            {
+                keys_buffer.push("[SHIFT]");
+                specs.shift = true;
+            }
+            else if (item[0] === "leftcontrol" || item[0] === "rightcontrol")
+            {
+                keys_buffer.push("[CTRL]");
+                specs.ctrl = true;
+            }
+            else if (item[0] === "leftalt" || item[0] === "rightalt")
+            {
+                keys_buffer.push("[ALT]");
+                specs.alt = true;
+            }
+            else
+                keys_buffer.push(item[0].toUpperCase());
             if (keys_buffer.length > 10)
+            {
                 keys_buffer.shift();
-            updateDisplay(keys_buffer);
-            console.log(`Added: ${item[0]}`);
-            // console.log(`${item[0]} | ${item[1]}`);
-            // letter.set(item[0])
+                keys_buffer.shift();
+            }
         }
-        else
-            console.log("Up");
+        else if (item[1] === "up")
+        {
+            if (item[0] === "leftcontrol" || item[0] === "rightcontrol")
+                specs.ctrl = false;
+            if (item[0] === "leftshift" || item[0] === "rightshift")
+                specs.shift = false;
+            if (item[0] === "leftalt" || item[0] === "rightalt")
+                specs.alt = false;
+        }
+        letter.set(keys_buffer.join(""))
     });
-
-    return <box>
-        <label label={bind(letter)}></label>
+    
+    return <box className="main-box">
+        <box>
+            <label label={bind(letter)}></label>
+        </box>
     </box>
 }
